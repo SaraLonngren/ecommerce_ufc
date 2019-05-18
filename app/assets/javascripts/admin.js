6b7de0a682af9d1eba50928d7020e2975c7d7f83
Claudio <claudioguimaraes14@gmail.com>
2019-05-18 16:36:50 -0300

    Admin layout

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,6 @@
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require app
+//= require bootstrap-sprockets
--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ Bundler.require(*Rails.groups)
 module Ecommerce
   class Application < Rails::Application
 
-    config.to_prepare do
-      Devise::SessionsController.layout 'admin_lte_2_login'
-    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
