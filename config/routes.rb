45a4291ff44b7923b803e357f07cdc036549827a
Claudio <claudioguimaraes14@gmail.com>
2019-05-18 16:09:46 -0300

    User cryd

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
     resources :categories
+    resources :users
     root 'dashboard#index'
   end
   root 'dashboard#index'
