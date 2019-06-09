37e867e6de173520169b95617a62371f7e886d16
Sara Lonngren <saralonngren@gmail.com>
2019-06-09 18:14:29 -0300

    Tentativas de criar o search no index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,24 @@ class ProductsController < ApplicationController
   end
 
   def query
+    @product = Product.new(product_params)
+    @products = Product.where(category_id: @product.category_id)
+
+    redirect_to search_path(@products)
   end 
 
   def search
   end 
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :category_id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   get '/checkout/edit', to: 'checkout#edit'
   patch '/checkout/update', to: 'checkout#update'
   get '/search', to:'products#search', as:'search'
-  post '/search', to:'produtcs#query', as:'query'
+  post '/search', to:'products#query', as:'query'
   
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
